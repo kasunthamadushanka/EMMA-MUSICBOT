3518da0bb02b3c0aae8313884358818d2fc23a17
KASU BRO <79846047+kasunthamadushanka@users.noreply.github.com>
2021-07-18 14:34:25 +0530

    Update SongPlayRoBot.py

--- SongPlayRoBot.py
+++ SongPlayRoBot.py
@@ -37,14 +37,13 @@ def start(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                     [                   [
-                        [
+                     [  
+                         
                             InlineKeyboardButton('👥 SUPPORT GROUP 👥', url='https://t.me/epusthakalayabotsupport'),
                             InlineKeyboardButton('📣 BOT UPDATES 📣', url='https://t.me/epusthakalaya_bots')
 
-                            InlineKeyboardButton('👤 DEV 👤', url='https://t.me/kasu_bro'),
+
                             InlineKeyboardButton('⚜️ ADD ME ⚜️', url='https://t.me/EmmaMusicBot?startgroup=true')
-                        ]
                     ]        
                 ]
             ]
